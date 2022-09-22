29636ec33b0f24f661bb33302ce6481328b46b43
Pedro Rodrigues <pedrodesarodrigues@gmail.com>
2022-09-22 12:01:45 -0300

    Part 2 - Jwt Configurations

---
+++ BuberDinner/BuberDinner.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+namespace BuberDinner.Application.Common.Interfaces.Authentication;
+
+public interface IJwtTokenGenerator
+{
+    string GenerateToken(
+        Guid userId,
+        string firstName,
+        string lastName);
+}
